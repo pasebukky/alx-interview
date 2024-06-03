92cadb493ddb435e551822a9a60485798fa3a847
pasebukky <olubukunolapase@gmail.com>
2024-06-03 12:44:29 +0100

    Lockboxes interview task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+        a method that determines if all the boxes can be opened.
+
+        return: True or False
+    """
+    if len(boxes) == 0:
+        return False
+
+    keys = {0}
+
+    visited = set()
+
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        visited.add(box)
+
+        for key in boxes[box]:
+            keys.add(key)
+            if key not in visited:
+                stack.append(key)
+
+    return len(visited) == len(boxes)
