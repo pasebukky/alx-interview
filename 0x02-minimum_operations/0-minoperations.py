790f149c5ffef5463f7f55bc80946e3c99ffbe34
pasebukky <olubukunolapase@gmail.com>
2024-06-11 23:13:51 +0100

    Solution to task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Create a minimum operator function that returns an integer
+"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+    while factor * factor <= n:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
