fb9e7293855dc3ac38b52ecd12c527e03245d472
pasebukky <olubukunolapase@gmail.com>
2024-07-09 09:54:51 +0100

    Script that prints all characters of a Star Wars movie

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data. Status code:', response.statusCode);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error('Failed to fetch data. Status code:', response.statusCode);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,9 @@
+This directory contains the solution to the ALX interview task 0x06. Star Wars API. The task is as follows:
+
+## 0-starwars_characters.js 
+Write a script that prints all characters of a Star Wars movie:
+
+The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+Display one character name per line in the same order as the “characters” list in the /films/ endpoint
+You must use the Star wars API
+You must use the request module
